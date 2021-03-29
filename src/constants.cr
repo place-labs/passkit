19a722da0eac11a655f5bfa4f28c34388711b687
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-03-29 12:00:01 +1100

    refactor(constants.cr): remove nillable type on env union and make read_base64_key private static

--- src/constants.cr
+++ src/constants.cr
@@ -1,10 +1,10 @@
 module PassKit
-  SIGNING_CERT         = read_base64_key(ENV["SIGNING_CERT"]?)
-  PRIVATE_KEY          = read_base64_key(ENV["PRIVATE_KEY"]?)
-  PRIVATE_KEY_PASSWORD = read_base64_key(ENV["PRIVATE_KEY_PASSWORD"]?)
-  WWDR_CERT            = read_base64_key(ENV["WWDR_CERT"]?)
+  SIGNING_CERT         = read_base64_key(ENV["SIGNING_CERT"])
+  PRIVATE_KEY          = read_base64_key(ENV["PRIVATE_KEY"])
+  PRIVATE_KEY_PASSWORD = ENV["PRIVATE_KEY_PASSWORD"]
+  WWDR_CERT            = read_base64_key(ENV["WWDR_CERT"])
 
-  def read_base64_key(key : String | Nil) : String
+  private def self.read_base64_key(key : String) : String
     raise ArgumentError.new("Base64-encoded Key is empty") unless key.presence
     String.new(Base64.decode(key))
   end
