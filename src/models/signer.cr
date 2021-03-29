437772d073577683a96d31f0f64ff65035c2b108
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-03-29 12:00:20 +1100

    refactor(signer.cr): remove check_config_file method

--- src/models/signer.cr
+++ src/models/signer.cr
@@ -10,10 +10,10 @@ class PassKit::Signer
   @wwdr_cert : String
 
   def initialize
-    @signing_cert = check_config_file(PassKit::SIGNING_CERT, "SIGNING_CERT")
-    @private_key = check_config_file(PassKit::PRIVATE_KEY, "PRIVATE_KEY")
+    @signing_cert = PassKit::SIGNING_CERT
+    @private_key = PassKit::PRIVATE_KEY
     @private_key_password = PassKit::PRIVATE_KEY_PASSWORD
-    @wwdr_cert = check_config_file(PassKit::WWDR_CERT, "WWDR_CERT")
+    @wwdr_cert = PassKit::WWDR_CERT
   end
 
   def sign(data) : String
@@ -21,11 +21,5 @@ class PassKit::Signer
     signed_data = `openssl smime -binary -sign -certfile #{@wwdr_cert} -signer #{@signing_cert} -inkey #{@private_key} -in #{data_file.path} -outform DER -passin pass:#{@private_key_password}`
     data_file.delete
     signed_data
-  end
-
-  private def check_config_file(path : String | Nil, name : String) : String
-    raise Error::MissingConfiguration.new "missing ENV configuration for #{name}" if path.nil?
-    raise Error::MissingFile.new "file does not exist #{path} for #{name}" unless File.exists?(path)
-    path
   end
 end
