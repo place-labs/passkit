a45e9c4684f174bd608856d83d28c77dc687f42d
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-03-30 16:47:52 +1100

    build(shard.yml): use habitat, improve env key check presence

--- src/constants.cr
+++ src/constants.cr
@@ -1,11 +1,15 @@
-module PassKit
-  SIGNING_CERT         = env_base64("SIGNING_CERT")
-  PRIVATE_KEY          = env_base64("PRIVATE_KEY")
-  WWDR_CERT            = env_base64("WWDR_CERT")
-  PRIVATE_KEY_PASSWORD = ENV["PRIVATE_KEY_PASSWORD"]
+require "habitat"
 
-  private def self.env_base64(key : String) : String
-    raise ArgumentError.new("Base64-encoded ENV #{key} is empty") unless ENV[key].presence
+module PassKit
+  Habitat.create do
+    setting signing_cert : String = PassKit.env_base64("SIGNING_CERT")
+    setting private_key : String = PassKit.env_base64("PRIVATE_KEY")
+    setting wwdr_cert : String = PassKit.env_base64("WWDR_CERT")
+    setting private_key_password : String = ENV["PRIVATE_KEY_PASSWORD"]
+  end
+
+  protected def self.env_base64(key : String) : String
+    raise ArgumentError.new("Base64-encoded ENV #{key} is empty") unless ENV[key]?.presence
     String.new(Base64.decode(ENV[key]))
   end
 end
